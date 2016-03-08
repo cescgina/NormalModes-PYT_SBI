6ff8773e409fcadaf5190cac338de9fb4987be2b
davidmasp <david.mas.p@gmail.com>
2016-03-08 22:37:40 +0100

    minor changes in my script to follow the stetics __ Also added a chech when getting the structure from the pdb file to check if the structure is a solution NMR

--- module_david.py
+++ module_david.py
@@ -1,36 +1,39 @@
+"""
+This module is a first version of some interestiing function based on biopython
+made by DMP.
+"""
 ### MODULE 1 ####
 
 from Bio.PDB import *
-import sys
-import re
 
-def isPDBcode(code):
-    if len(code)==4:
+def pdb_code_check(code):
+    """Checks if the code provided have 4 letters"""
+    if len(code) == 4:
         return code
     else:
         raise ValueError
 
 
+def nmr_check(structure):
+    """
+    From a structure instance it returns True if the structure_method is
+    'solution nmr'
+    """
+    if structure.header['structure_method'] == 'solution nmr':
+        return True
+    else:
+        raise ValueError
 
 def from_pdb_code_to_structure(code):
     """
     From a specific pdb code this function retrieves the structure file
     from the web database and generated a structure instance.
     """
-    code=isPDBcode(code)
+    code = pdb_code_check(code)
     pdbl = PDBList()
-    parser=PDBParser()
-    structure=parser.get_structure(code,pdbl.retrieve_pdb_file(code))
-    return structure
-
-
-
-def isNMR(structure):
-    """
-    From a structure instance it returns True if the structure_method is
-    'solution nmr'
-    """
-    if structure.header['structure_method']=='solution nmr':
-        return True
+    parser = PDBParser()
+    structure = parser.get_structure(code, pdbl.retrieve_pdb_file(code))
+    if nmr_check(structure):
+        return structure
     else:
-        raise ValueError
+        return ValueError
