c5316aee4f4e3663dbad25f71dcbb0bd72d85a84
davidmasp <david.mas.p@gmail.com>
2016-03-13 10:16:25 +0100

    small change in the importation to let the program work in the mac

--- edanalysis.py
+++ edanalysis.py
@@ -1,6 +1,8 @@
 import Bio.PDB as pdb
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt
 from scipy import linalg
 
 
