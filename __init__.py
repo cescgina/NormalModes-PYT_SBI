b1ff584fda4ef5985f19fea90e4c98ec6d180902
davidmasp <david.mas.p@gmail.com>
2016-03-17 10:53:37 +0100

    prototypes of init and setup

---
+++ __init__.py
@@ -0,0 +1,12 @@
+PyEDA/                          Top-level package
+      __init__.py               Initialize PyEDA package
+      interface/                 Subpackage to generate a tkinter interface
+              __init__.py
+              interface.py
+      analysis/                  Subpackage to perform the mathematical analysis
+              __init__.py
+              edanalysis.py
+      helper_functions/          Subpackage to help the other functions
+              __init__.py
+              module_david.py
+              project_jf.py
--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name='PyEDA',
       author='JF Gilabert & D Mas',
       author_email='joanfrancesc.gilabert01@estudiant.upf.edu & david.mas01@estudiant.upf.edu',
       url='http://bit.ly/PyEDA',
-      packages=['distutils', 'distutils.command'],
+      packages=['PyEDA_modules', 'PyEDA_modules.interface', 'PyEDA_modules.analysis', 'PyEDA_modules.helper_functions'],
      )
